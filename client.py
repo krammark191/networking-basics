a20b338e0fe7eb96decb359eabd2dfc4a9bf4c43
Mark Van Horn <krammark191@gmail.com>
2022-06-22 22:18:22 -0600

    Added comments

--- client.py
+++ client.py
@@ -1,28 +1,54 @@
 import socket
 import pickle
 
+# Set constant values.
 HEADER_SIZE = 10
 PORT_NUMBER = 9999
-SERVER_IP = '10.244.37.20' # Replace with server IP address.
+SERVER_IP = '10.36.37.83' # Replace with server IP address.
 
+# Open a socket and connect to the server.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_IP, PORT_NUMBER))
+s.connect((socket.gethostname(), PORT_NUMBER))
 
 
 while True:
+
+    # Initialize full_msg and new_msg variables.
     full_msg = b''
     new_msg = True
+
+    # Begin receive loop.
     while True:
+
+        # Set buffer size of 16 bytes.
         msg = s.recv(16)
+
+        # Checks if their is still information to be received.
         if new_msg:
+
+            # Display the header and the message size.
             print(f"new message length: {msg[:HEADER_SIZE]}")
             msg_length = int(msg[:HEADER_SIZE])
+
+            # Set new_msg to False as it is no longer the first buffer of the message.
             new_msg = False
+
+        # Add the first buffer of the message to the full message.
         full_msg += msg
+
+        # Checks to see if the message is fully received.
         if len(full_msg) - HEADER_SIZE == msg_length:
+
+            # Prints confirmation of message received.
             print("full msg recvd")
             print(full_msg[HEADER_SIZE:])
+
+            # Decrypts pickle message from byte format to python object.
             d = pickle.loads(full_msg[HEADER_SIZE:])
+
+            # Prints the decrypted message.
             print(d)
+
+            # Sets variables back to their default states and awaits the next message.
             new_msg = True
             full_msg = b''
--- server.py
+++ server.py
@@ -1,21 +1,46 @@
 import socket
 import pickle
 
+# Set constant values.
 HEADER_SIZE = 10
 PORT_NUMBER = 9999
 
+# Grabs the IP address that the server is located
+# on and stores it in the host_name variable.
 host_name = socket.gethostbyname(socket.gethostname())
 
+# Creates a socket object and binds the server
+# to the local IP address and sets the port number.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host_name, PORT_NUMBER))
 s.listen(5)
 
+# Begins send loop.
 while True:
+
+    # Connects to any client requests and saves the values.
     clientSocket, address = s.accept()
+
+    # Prints confirmation message that a
+    # client is connected and prints the address.
     print('Connected to', address)
 
-    d = {1: "Hello", 2: "World", 3: "!"}
+    # Creates a python object.
+    d = {"Test 1": [1, 2, 3, 4, 5],
+         "Test 2": [
+        5, 4, 3, 2, 1],
+        "Test 3": True,
+        "Test 4": False,
+        "Test 5": 5,
+        "Test 6": 6.0,
+        "Test 7": {1: 1, 2: 2, 3: 3}}
+
+    # Compresses the python object into a
+    # sendable format with the pickle library.
     msg = pickle.dumps(d)
 
+    # Converts the message header to byte format and appends the pickle object.
     msg = bytes(f"{len(msg):<{HEADER_SIZE}}", "utf-8") + msg
+
+    # Sends the message to the client.
     clientSocket.send(msg)
